0644852455ef25bfb22545016edf3b655ca6f93a
zhuhong1 <zhuhong1@guahao.com>
2018-01-31 17:34:26 +0800

    20180131-create spring-boot example project

---
+++ spring-boot/spring-boot-examples/src/main/java/cn/baiyang/origin/spring/boot/examples/SampleController.java
@@ -0,0 +1,28 @@
+package cn.baiyang.origin.spring.boot.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author hongzhu
+ * @version V1.0
+ * @since 2018-01-30 19:18
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
